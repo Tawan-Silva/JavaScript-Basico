370f20e67ca09bead883445a3bffbd76318d5b2b
Tawan-Silva <63369670+Tawan-Silva@users.noreply.github.com>
2022-04-20 18:05:08 -0300

    :up: Novas Aulas

--- index.js
+++ index.js
@@ -21,29 +21,21 @@
 
 // segundaFuncao()
 
-// function getUser(id) {
-//   return fetch(`http://regres.in/api/users?id=${id}`)
-//   .then(data => data.json())
-//   .catch(err => console.log(err))
-// }
+async function getUser(id) {
+  return fetch(`http://regres.in/api/users?id=${id}`)
+  .then(data => data.json())
+  .catch(err => console.log(err))
+}
 
-// async function showUserName(id) {
-//   const user = await getUser(id);
+await function showUserName(id) {
+  const user = await getUser(id);
 
-//   console.log(`O nome do usuário é: ${user.data.fisrt_name}`);
-// }
+  console.log(`O nome do usuário é: ${user.data.fisrt_name}`);
+}
 
-// showUserName(3)
 
-// const numeros = [1, 2, 3, 4, 5, 6];
-// const numerosPares = []
-// function encontraPares() {
-//     for (let i = 0; i < numeros.length; i++) {
-//         if (numeros[i] % 2 === 0) numerosPares.push(numeros[i])
-//     }
-//     return numerosPares
-// }
-// console.log(encontraPares());
+
+
 
 
 
